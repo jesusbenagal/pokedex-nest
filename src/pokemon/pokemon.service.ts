229148abcdc9e8ec0700eb5258d3523d1988d337
Jesús Guerrero <jesus.guerrero@talan.com>
2022-09-14 17:03:12 +0200

    Updating pokemon and create handleException method

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -23,15 +23,7 @@ export class PokemonService {
       const pokemon = await this.pokemonModel.create(createPokemonDto);
       return pokemon;
     } catch (err) {
-      if (err.code === 11000) {
-        throw new BadRequestException(
-          `Pokemon exists in db ${JSON.stringify(err.keyValue)}`,
-        );
-      }
-      console.log(err);
-      throw new InternalServerErrorException(
-        `Can't create pokemon - Check server logs`,
-      );
+      this.handleExceptions(err);
     }
   }
 
@@ -66,11 +58,30 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+
+    try {
+      await pokemon.updateOne(updatePokemonDto);
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+    } catch (err) {
+      this.handleExceptions(err);
+    }
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(`Check server logs`);
   }
 }
